c60c4bb6280c488fe4985d2432dcc126c5a20672
hbl917070 <hbl917070@hotmail.com.tw>
2017-06-28 02:19:56 +0800

    Add files via upload

--- 巴哈-過濾-熱門推薦.js
+++ 巴哈-過濾-熱門推薦.js
@@ -18,7 +18,7 @@
 
 
 //要排除的關鍵字（包含這些文字的文章及就看不到圖片
-var ar_out = new Array("寶", "寳", "宝", "polla", "月經", "戰象", "狂猴", "fbi", "吉娃", "胡屠戶");
+var ar_out = new Array("寶", "寳", "宝", "polla", "月經", "戰象", "狂猴", "fbi", "吉娃", "瓦", "胡屠戶");
 
 //被過濾的圖片會被替換成這個網址
 var s_img_url = "data:image/svg+xml;base64,PHN2ZyAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgMTA5Ljk1IDEwOS45NSI+PHN0eWxlPi5cMzcgOWMwYmEwY3tmaWxsOm5vbmU7c3Ryb2tlOiNlNjAwMTI7c3Ryb2tlLW1pdGVybGltaXQ6MTA7c3Ryb2tlLXdpZHRoOjRweDt9PC9zdHlsZT48bGluZSBjbGFzcz0iNzljMGJhMGMiIHgxPSIxLjQxIiB5MT0iMS40MSIgeDI9IjEwOC41NCIgeTI9IjEwOC41NCIvPjxsaW5lIGNsYXNzPSI3OWMwYmEwYyIgeDE9IjEuNDEiIHkxPSIxMDguNTQiIHgyPSIxMDguNTQiIHkyPSIxLjQxIi8+PC9zdmc+";
